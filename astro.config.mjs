024ab6bf495e17e383dcf9f5a71e350a50c2e10c
tuerre <jendryjhancel@gmail.com>
2025-07-07 13:36:24 -0400

    chore: update to tailwind v3 to use react islands and @astro/tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-// @ts-check
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-// https://astro.build/config
+
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [react(), tailwind()],
 });
