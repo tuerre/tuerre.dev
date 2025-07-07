7de1615f8d1e5204fd8e0b4d46ccbdcd86e0cf2a
tuerre <jendryjhancel@gmail.com>
2025-07-07 14:26:04 -0400

    chore: remove react and jsrepo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
 });
