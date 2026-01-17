c54d48552066dbe4c84f5511d8b91b259fe3cfcc
tuerre <jendryjhancel@gmail.com>
2026-01-17 18:41:16 -0400

    feat: Add Habilidades section with skill icons and their mapping.

--- src/components/IconMap.ts
+++ src/components/IconMap.ts
@@ -5,21 +5,20 @@ import CSS from './icons/css.astro'
 import Git from './icons/git.astro'
 import HTML5 from './icons/html.astro'
 import MySQL from './icons/mysql.astro'
-import Figma from './icons/figma.astro'
 import Csharp from './icons/csharp.astro'
 import NodeJS from './icons/nodejs.astro'
 import GitHub from './icons/GitHub.astro'
-import VSCode from './icons/vscode.astro'
 import AstroIcon from './icons/astro.astro'
-import Windows from './icons/windows.astro'
 import Tailwind from './icons/tailwind.astro'
-import Windsurf from './icons/windsurf.astro'
 import Supabase from './icons/supabase.astro'
 import SqlServer from './icons/sqlserver.astro'
-import DiscordJS from './icons/discordjs.astro'
 import JavaScript from './icons/javascript.astro'
 import TypeScript from './icons/typescript.astro'
-import VisualStudio from './icons/visualstudio.astro'
+import PostgreSql from './icons/postgresql.astro'
+import NextJS from './icons/nextjs.astro'
+import ReactJS from './icons/reactjs.astro'
+import VSCode from './icons/vscode.astro'
+import ExpressJS from './icons/expressjs.astro'
 
 export const iconMap: Record<string, AstroComponentFactory> = {
   'c++': Cpp,
@@ -36,10 +35,10 @@ export const iconMap: Record<string, AstroComponentFactory> = {
   'astro': AstroIcon,
   'git': Git,
   'github': GitHub,
-  'discordjs': DiscordJS,
-  'figma': Figma,
+  'postgresql': PostgreSql,
+  'nextjs': NextJS,
+  'reactjs': ReactJS,
+  'react-native': ReactJS,
   'vscode': VSCode,
-  'visualstudio': VisualStudio,
-  'windsurf': Windsurf,
-  'windows': Windows,
+  'expressjs': ExpressJS,
 }
